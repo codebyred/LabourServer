d0f090229e22b54f480de4b46c6db2281a0dd5fc
Redoan <nazmulhaqueredoan@gmail.com>
2024-03-21 16:30:00 +0600

    feat: add route handler to delete and post worker

--- src/Controllers/worker.controller.js
+++ src/Controllers/worker.controller.js
@@ -28,5 +28,61 @@ export const getWorker = async (req,res)=>{
 
     res.json(worker);
 
+}
+
+export const postWorker = async (req, res)=>{
+
+    try{
+
+        const {id, firstName, lastName, email, password, category_id} = await req.body;
+
+        const worker = await Worker.build({
+            id: id, 
+            firstName: firstName, 
+            lastName: lastName, 
+            email: email, 
+            password: password, 
+            category_id: category_id
+        });
+
+        await worker.save();
+
+        return res.json({success:1, msg: "worker successfully saved"})
+
+    }catch(e){
+
+        return res.json({success:0, msg: "worker could not be saved"});
+
+    }
+
+
+}
+
+export const deleteWorker = async (req, res)=>{
+
+    const {id} = req.params;
+
+    console.log(id);
+
+    try{
+
+        const worker = await Worker.findOne({
+            where:{
+                id
+            }
+        });
+
+        if(!worker) throw new Error("worker not found");
+
+        await worker.destroy();
+
+        return res.json({success:1 , msg: "Woker deleted"});
+
+    }catch(e){
+        console.log(e);
+        return res.json({success:0, msg: "Can not delete worker"});
+    }
+
+
 
 }
--- src/Routes/worker.route.js
+++ src/Routes/worker.route.js
@@ -1,9 +1,11 @@
 import express from "express"
-import {getWorkers, getWorker} from "../Controllers/worker.controller.js";
+import {getWorkers, getWorker, postWorker, deleteWorker} from "../Controllers/worker.controller.js";
 
 const router = express.Router();
 
-router.get("/",getWorkers);
-router.get("/:email",getWorker);
+router.get("/", getWorkers);
+router.get("/:email", getWorker);
+router.post("/", postWorker);
+router.delete("/:id", deleteWorker);
 
 export default router;
