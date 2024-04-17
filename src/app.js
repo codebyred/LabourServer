fc6a135dbd005eef2e5450766b8c188b407d687a
Redoan <nazmulhaqueredoan@gmail.com>
2024-04-18 00:22:41 +0600

    feat: add registration with apigateway

--- src/app.js
+++ src/app.js
@@ -12,8 +12,46 @@ app.use(express.json());
 
 app.use("/api/",routes);
 
-app.listen(port,(err) =>{
-    if(err) console.log(`Could not establish connection on ${port}`);
+app.listen(port, async (err) =>{
+    if(err) return console.log(`Could not establish connection on ${port}`);
+
     console.log(`connection is established on ${port}`);
+
+    const register = async()=>{
+
+        const body = {
+
+            service:"worker",
+            host:"http://localhost",
+            port,
+            url:`http://localhost:${port}`
+    
+        }
+    
+        const fetchOptions = {
+            method: 'POST',
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify(body),
+            credentials: "include",
+        }
+    
+        const apiGatewayURL = "http://localhost:3000/register";
+
+        await fetch(apiGatewayURL, fetchOptions);
+   
+    }
+
+    try{    
+
+        await register();
+
+    }catch(e){
+
+        console.log("Could not register to apiGateway");
+        
+    }
 });
 
